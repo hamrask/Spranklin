db31ea2500f3ccea9dc0d9fb14db9af7577864c2
hamraz <khamras@gmail.com>
2020-02-20 22:06:57 +0530

    added titlebar and sidebar

--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TitlebarComponent } from 'src/app/shared/components/titlebar/titlebar.component';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TitlebarComponent , FooterComponent , SidebarComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatFormFieldModule
   ]
 })
 export class HomeModule { }
